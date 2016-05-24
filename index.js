ce24d74885cd846f45ed95e77e98db1200b7728c
Adam Jones <adam@modsrus.com>
2016-05-24 04:44:02 -0400

    Major refactor: device objects, device config loading, 'on' removed, disconnect added

--- index.js
+++ index.js
@@ -12,9 +12,84 @@ var defaults = {
 	}
 };
 
-function Outlet(opt)
+
+function registerListener(handler, emit, trigger, func) {
+	emit = emit ? emit : "on";
+
+	return handler[emit](trigger, func);
+}
+
+function unregisterListener(handler, func) {
+	return handler.off(func);
+}
+
+function Device(nameIn, dirIn)
+{
+	var name = nameIn;
+	var dir = dirIn; 
+	var description, conf, uses, listeners, keywords;
+
+	return({
+		name: name,
+		dir: dir,
+		description: description,
+		conf: conf,
+		uses: uses,
+		listeners: listeners,
+		keywords: keywords,
+		load: load
+	});
+
+	function load() {
+		var device = this;
+		var package;
+
+
+		if (!_.isEmpty(device.dir)) {
+			package = require(path.join(device.dir, '/package.json'));
+		}
+		else {
+			if (_.isEmpty(device.name)) {
+				throw new Error('Device name or path must be specified');
+			}
+
+			try {
+				device.dir = path.join(process.cwd(), '/node_modules/', device.name);
+				package = require(path.join(device.dir, '/package.json'));
+			}
+			catch (err) {
+				// TODO: from config
+				try {
+					device.dir = path.join(process.cwd(), defaults.connect.path, device.name);
+					package = require(path.join(this.dir, '/package.json'));
+				}
+				catch (e) {
+					console.error('Device not found: '+device.name);
+				}
+			}
+		}
+
+		device = _.assign(device,
+			require(device.dir),
+			_.pick(package, ['name', 'version', 'description', 'keywords']),
+			_.pick(package.device, ['conf', 'uses', 'listeners'])
+			);
+		
+		// remove prefix (ie. device-test -> test)
+		device.name = _.trimStart(device.name, defaults.connect.prefix);
+
+		_.forEach(device, function(prop) {
+			this[prop] = prop;
+		});
+
+		return this;
+	}
+}
+
+function Outlet(opt) 
 {
 	var devices = [];
+	var registry = {};
 
 	// init new event emitter
 	var handler = ee();
@@ -30,73 +105,80 @@ function Outlet(opt)
 		handler: handler,
 		devices: devices,
 		connect: connect,
-		on: on,
+		disconnect: disconnect,
 		trigger: trigger
 	});
 
-	function trigger(event) {
-		var args = _.drop(arguments, 1);
+	function trigger(event, args) {
+		// init if not defined
+		args = args?args:{};
+		// add extra args passed
+		args.args = _.drop(arguments, 2);
 
-		handler.emit(event, args);
-
-		return this;
-	}
-
-	function load(device) {
-		if (!device.path) {
-			throw new Error('Device not connected: '+JSON.stringify(device));
-		}
-
-		device.device = require(device.path);
-
-		return device;
-	}
-
-	function on(name, args) {
-		args = args ? args : {};
-
-		// turn on all devices
-		if (!name || name == '*') {
-			_.forEach(devices, function(device) {
-				var onArgs = args;
-				onArgs.name = device.name;
-				onArgs.path = device.path;
-				device = load(device);
-				if (device.device.on) {
-					device.device.on(this, onArgs);
-				}
-			});
-		}
-
-		// TODO: find device by name
+		handler.emit(event, this, args);
 
 		return this;
 	}
 
 	function connect(name) {
-		var device = null;
+		// find & require module
+		var device = Device(name).load();
 
-		var dir = path.join(process.cwd(), conf.get('connect:path'), name);
-
-		var moduleName = conf.get('connect:prefix')+name;
-		var moduleDir = path.join(process.cwd(), '/node_modules/', moduleName);
-
-		try {
-			device = {
-				name: name,
-				path: moduleDir
-			};
-			device.package = require(path.join(moduleDir, '/package.json'));
-		}
-		catch (e) {
-			device = {
-				name: name,
-				path: dir
-			};
-			device.package = require(path.join(dir, '/package.json'));
+		// register all listeners
+		if (_.isArray(device.listeners)) {
+			_.forEach(device.listeners, function(listener) {
+				registerListener(handler, listener.emit, listener.trigger, device[listener.action]);
+			});
 		}
 
+		// FIXME
+		var dconf = {"device": {}[device.name] = device.conf};
+		this.conf.defaults(dconf);
+
+		// add device to registry
 		devices.push(device);
+		registry[device.name] = devices.length-1;
+
+		// fire connect action if defined
+		if (_.isFunction(device.connect)) {
+			device.connect(this, {name: name, dir: device.dir});
+		}
+
+		// emit global connect event
+		trigger('connect', {device: device});
+
+		return this;
+	}
+
+	function disconnect(name) {
+		// find device
+		var device = devices[registry[name]];
+
+		if (!_.isObject(device)) {
+			throw new Error("Device not found: "+name);
+		}
+
+		// un-register all listeners
+		if (_.isArray(device.listeners)) {
+			_.forEach(device.listeners, function(listener) {
+				// FIXME
+				//unregisterListener(handler, device[listener.action]);
+			});
+		}
+
+		// remove device from registry
+		this.devices = _.pullAt(devices, registry[name]+1);
+		delete registry[name];
+
+		// fire disconnect action if defined
+		if (_.isFunction(device.disconnect)) {
+			device.disconnect(this, {name: name, dir: device.dir});
+		}
+
+		// emit global disconnect event
+		trigger('disconnect', {device: device});
+
+		device = null;
 
 		return this;
 	}
